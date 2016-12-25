fb7d82e34bc43cd5b4a43d8bb9a964c2dca5e1bb
Jacob Laney <jake.laney96@gmail.com>
2016-12-24 20:26:26 -0600

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Jake Laney on 12/24/16.
+//  Copyright © 2016 Jake Laney. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
